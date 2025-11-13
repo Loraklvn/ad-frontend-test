44801757f03e15e16eea399e043e62bd7b219018
Kelvin Lora <kelvinlora5@gmail.com>
2025-11-13 15:06:05 -0400

    chore(tailwind): initialize tailwind configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "#8F8F8F",
+        primary: "#585660",
+        secondary: "#737373",
+      },
+    },
+  },
+} satisfies Config;
